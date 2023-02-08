9cf7dae32ea927b66edc2f3c362c559bedb8f47c
keerthilakshmisreegantasala <84431893+keerthilakshmisreegantasala@users.noreply.github.com>
2023-02-08 08:22:37 +0530

    Create Town Breakers.java

---
+++ 7/2/2023/week-2/Town Breakers.java
@@ -0,0 +1,9 @@
+public static int towerBreakers(int n, int m) {
+    // Write your code here
+        if(m==1) 
+            return 2;
+        if(n%2==1)
+            return 1;
+        else 
+            return 2;
+    }

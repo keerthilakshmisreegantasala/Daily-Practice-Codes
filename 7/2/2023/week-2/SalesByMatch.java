da786b85d23c948eb9ab78dba68ce972bcbf3866
keerthilakshmisreegantasala <84431893+keerthilakshmisreegantasala@users.noreply.github.com>
2023-02-08 08:19:03 +0530

    Create SalesByMatch.java

---
+++ 7/2/2023/week-2/SalesByMatch.java
@@ -0,0 +1,15 @@
+public static int sockMerchant(int n, List<Integer> ar) {
+    // Write your code here
+      int count=0;
+      HashSet<Integer>h=new HashSet<>();
+      for(int i=0;i<n;i++){
+          if(!h.contains(ar.get(i))){
+              h.add(ar.get(i));
+          }
+          else{
+              h.remove(ar.get(i));
+              count++;
+          }
+      }
+      return count;
+    }

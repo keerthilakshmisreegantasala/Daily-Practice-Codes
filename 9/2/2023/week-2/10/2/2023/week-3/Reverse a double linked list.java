61f46dbc5a15cb3cd9efe16c251239d9636d827c
keerthilakshmisreegantasala <84431893+keerthilakshmisreegantasala@users.noreply.github.com>
2023-02-13 12:01:49 +0530

    Create Reverse a double linked list.java

---
+++ 9/2/2023/week-2/10/2/2023/week-3/Reverse a double linked list.java
@@ -0,0 +1,18 @@
+public static DoublyLinkedListNode reverse(DoublyLinkedListNode llist) {
+    // Write your code here
+     DoublyLinkedListNode current=llist;
+       DoublyLinkedListNode temp=null;
+       DoublyLinkedListNode  prev=null;
+       if(llist==null || llist.next==null){
+           return llist;
+       }
+        while (current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+        llist=temp.prev;
+        return llist;
+    }
+}

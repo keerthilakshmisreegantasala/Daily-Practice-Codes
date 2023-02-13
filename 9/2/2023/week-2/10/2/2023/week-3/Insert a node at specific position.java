f78e3b2886cbbb6bd765075199a820193f3cb568
keerthilakshmisreegantasala <84431893+keerthilakshmisreegantasala@users.noreply.github.com>
2023-02-13 12:03:23 +0530

    Create Insert a node at specific position.java

---
+++ 9/2/2023/week-2/10/2/2023/week-3/Insert a node at specific position.java
@@ -0,0 +1,16 @@
+ if (llist == null) return null;
+        SinglyLinkedListNode temp = new SinglyLinkedListNode(data);
+        if (position == 0) {
+            temp.next = llist;
+            return temp;
+        }
+        SinglyLinkedListNode p = llist;
+        for (int i = 0; i< position-1; i++) {
+            p = p.next;
+        }
+        SinglyLinkedListNode next = p.next;
+        p.next = temp;
+        temp.next = next;
+        return llist;
+    }
+}
